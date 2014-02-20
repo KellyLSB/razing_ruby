0ac6a15cdcbd1b66b3b3ee28f16442da51a218b3
Kelly Becker <kellylsbkr@gmail.com>
2014-02-19 17:39:16 -0800

    Changing how the logger gets it's variables

--- lib/razing_ruby/logger.rb
+++ lib/razing_ruby/logger.rb
@@ -20,18 +20,11 @@ class Logger
   end
 
   class LogDevice
-    attr_reader :loggers
-
-    # Create the logger cache
-    def raze_initialize(*a, &b)
-      ruby_initialize(*a, &b)
-      @loggers ||= {}
-    end
 
     # Write a log
     def raze_write(*a, &b)
       results = [ruby_write(*a, &b)]
-      @loggers.values.map(&:logdev).reduce(results) do |out, logger|
+      loggers.values.map(&:logdev).reduce(results) do |out, logger|
         logger.respond_to?(:write) ? out << logger.write(*a, &b) : out
       end
     end
@@ -40,23 +33,25 @@ class Logger
     def add_logger(name, logger = nil)
       name, logger = __prep_logger__(name, logger)
       return false if logger.logdev == self
-      @loggers[name] = logger
+      loggers[name] = logger
     end
 
     # Remove a logger
     def del_logger(name)
-      @loggers[name].close
-      @loggers.delete(name)
+      loggers[name].close
+      loggers.delete(name)
     end
 
     # Get a logger
     def get_logger(name)
-      @loggers[name]
+      loggers[name]
+    end
+
+    def loggers
+      @loggers ||= {}
     end
 
     # Swap the methods with the raze ones
-    alias_method :ruby_initialize, :initialize
-    alias_method :initialize, :raze_initialize
     alias_method :ruby_write, :write
     alias_method :write, :raze_write
 
